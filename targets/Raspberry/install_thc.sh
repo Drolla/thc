94545a4b261eb9186c8e2d2ba2e39b30f6ea6f27
drolla <andreas@drollinger.ch>
2016-02-18 22:42:44 +0100

    Raspberry PI installer update

--- targets/Raspberry/install_thc.sh
+++ targets/Raspberry/install_thc.sh
@@ -5,30 +5,42 @@ if [ `whoami` != "root" ]; then
 	exit 1
 fi
 
-cd ..
+old_thc_dir=""
+if [ -d "/opt/thc" ]; then
+	echo "Existing THC installation found. Stop the THC process and archive the current installation"
+	/etc/init.d/thc.sh stop > /dev/null 2>&1
+	old_thc_dir=/opt/thc_$(date "+%Y-%m-%d-%H-%M-%S")
+	mv /opt/thc $old_thc_dir
+fi
 
-echo "Stop TightHomeControl service"
-/etc/init.d/thc.sh stop > /dev/null 2>&1
+cd ~
 
-echo "Copy thc.sh to etc/init.d, and register the service"
-cp bin/thc.sh /etc/init.d
-chmod 775 /etc/init.d/thc.sh
-update-rc.d thc.sh defaults
+echo "Install Tcl, RRDTools, mail send support"
+sudo apt-get install tcl rrdtool-tcl rrdtool ssmtp heirloom-mailx
 
-
-  > cd /etc/init.d
-  > sudo cp /opt/thc/targets/Raspberry/thc.sh .
-  > sudo chmod 775 ./thc.sh
-  > sudo update-rc.d thc.sh defaults 98 02
-
-echo "Install the TightHomeControl run directory (/opt/thc)"
-mkdir -p /opt/thc
-cp -r ../.. /opt
+echo "Install THC inside /opt/thc"
+wget -q -O /tmp/thc-master.zip https://github.com/Drolla/thc/archive/master.zip
+unzip -d /opt /tmp/thc-master.zip
+mv /opt/thc-master /opt/thc
 chmod 775 /opt/thc/bin/thc.tcl
 chmod 775 /opt/thc/targets/Raspberry/thc_shell_control.tcl
+
+if [ "$old_thc_dir" != "" ]; then
+	echo "Copy old THC configuration file into /opt/thc"
+	cp $old_thc_dir/config.tcl /opt/thc
+fi
+
+echo "Setup THC as a service"
+cd /etc/init.d
+sudo cp /opt/thc/targets/Raspberry/thc.sh .
+sudo chmod 775 ./thc.sh
+sudo update-rc.d thc.sh defaults 98 02
 
 echo "Create the log directory (/var/thc)"
 mkdir -p /var/thc
 
-echo "Start TightHomeControl service"
-/etc/init.d/thc.sh start
+echo "Perform the following configurations to complete the THC installation:"
+echo "* Edit the THC configuration file /opt/thc/config.tcl"
+echo "* Setup the mail service"
+echo "* Start THC as a service:"
+echo "  > /etc/init.d/thc.sh start"
