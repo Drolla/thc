a0e0b8b9d61780c9093cc6586737053dc451d2fc
Andreas Drollinger <andreas@drollinger.ch>
2016-11-14 21:37:35 +0100

    Package updates: t2ws, t2ws::bauth, t2ws::template

--- libs/t2ws/pkgIndex.tcl
+++ libs/t2ws/pkgIndex.tcl
@@ -1,3 +1,4 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded t2ws 0.4 [list source [file join $dir t2ws.tcl]]
-package ifneeded t2ws_template 0.2 [list source [file join $dir t2ws_template.tcl]]
+package ifneeded t2ws 0.6 [list source [file join $dir t2ws.tcl]]
+package ifneeded t2ws::template 0.4 [list source [file join $dir t2ws_template.tcl]]
+package ifneeded t2ws::bauth 0.2 [list source [file join $dir t2ws_bauth.tcl]]
