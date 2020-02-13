4bbe2e88f49d0f7721399588d1ae8c0c7448b97a
Andreas Drollinger <Andreas Drollinger>
2020-02-13 23:11:34 +0100

    Adapt to the new MeteoSwiss data portal.

--- modules/thc_MeteoSwiss.tcl
+++ modules/thc_MeteoSwiss.tcl
@@ -5,7 +5,6 @@
 # 
 # This module implements devices that get weather data from the MeteoSwiss
 # via the SwissMetNet data portal
-# (see: http://opendata.admin.ch/en/dataset/messdatensmn).
 #
 # Copyright (C) 2015 Andreas Drollinger
 ##########################################################################
@@ -18,12 +17,19 @@
 # Group: Introduction and setup
 #
 # The thc_MeteoSwiss module implements THC devices that get data from 
-# SwissMetNet. For details about the data portal, see 
-# http://opendata.admin.ch/en/dataset/messdatensmn.
+# SwissMetNet. For details about the data set, see
+# https://opendata.swiss/en/dataset/automatische-wetterstationen-aktuelle-messwerte
+# or
+# https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell.
 #
 # To get weather data you need first to know the abbreviation of the location
-# you are interested in. For this open data legend file http://data.geo.admin.ch.s3.amazonaws.com/ch.meteoschweiz.swissmetnet/03-10min_Daten_legenden.zip.
+# you are interested in. For this, open data legend file
+# https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/info/VQHA80_en.txt
 # 
+# The weather data is then extracted from the following file that is updated in
+# a regular interval :
+# https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv
+#
 # The following current weather parameters are available :
 #
 # * temperature - Air temperature 2 m above ground; current value (°C)
@@ -62,7 +68,8 @@ namespace eval ::thc::MeteoSwiss {
 		# Fetch the current weather data
 		# Get the current weather data for the location
 		catch {
-			set LocationResponse [::thc::GetUrl "http://data.geo.admin.ch.s3.amazonaws.com/ch.meteoschweiz.swissmetnet/VQHA69.csv"]
+			set LocationResponse [::thc::GetUrl \
+					"https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv"]
 			set LocationData [lindex $LocationResponse 2]
 		}
 			
@@ -70,16 +77,17 @@ namespace eval ::thc::MeteoSwiss {
 		# :  
 		# :  MeteoSchweiz / MeteoSuisse / MeteoSvizzera / MeteoSwiss
 		# :  
-		# :  stn|time|tre200s0|sre000z0|rre150z0|dkl010z0|fu3010z0|pp0qnhs0|fu3010z1|ure200s0|prestas0|pp0qffs0
-		# :  TAE|201411301700|2.0|0|0.0|62|5.0|1009.2|10.4|99|946.3|1011.2
-		# :  COM|201411301700|9.3|0|0.1|155|5.0|1011.6|6.8|97|944.3|1011.8
-		# :  ABO|201411301700|8.9|0|0.0|340|1.8|1008.7|3.2|62|859.7|-
-		# :  AIG|201411301700|7.7|0|0.0|250|0.7|1008.6|1.8|89|963.7|1009.2
+		# :  stn;time;tre200s0;sre000z0;rre150z0;dkl010z0;fu3010z0;pp0qnhs0;fu3010z1;ure200s0;prestas0;pp0qffs0
+		# :  TAE;201411301700;2.0;0;0.0;62;5.0;1009.2;10.4;99;946.3;1011.2
+		# :  COM;201411301700;9.3;0;0.1;155;5.0;1011.6;6.8;97;944.3;1011.8
+		# :  ABO;201411301700;8.9;0;0.0;340;1.8;1008.7;3.2;62;859.7;-
+		# :  AIG;201411301700;7.7;0;0.0;250;0.7;1008.6;1.8;89;963.7;1009.2
 		# :  ...
 		
 		# Identify the position of the different fields
 		catch {
-			set Fields [split [regexp -inline -line {^.*\ystn\y.*$} $LocationData] "|"]
+			set Fields [split [lindex \
+					[regexp -inline -line {^.*\ystn\y.*$} $LocationData] 0] ";"]
 		}
 		
 	 	# Extract from the data the requested parameters for the different 
@@ -89,8 +97,10 @@ namespace eval ::thc::MeteoSwiss {
 			catch {
 				set Location [lindex $GetCmd 0]
 				set Parameter [lindex $GetCmd 1]
-				set DataSet [split [regexp -inline -line "^$Location.*$" $LocationData] "|"]
-				set Res [lindex $DataSet [lsearch -exact $Fields $ParameterNames($Parameter)]] 
+				set DataSet [split [lindex \
+						[regexp -inline -line "^$Location.*$" $LocationData] 0] ";"]
+				set Res [lindex $DataSet \
+						[lsearch -exact $Fields $ParameterNames($Parameter)]] 
 			}
 			lappend Result $Res
 		}
